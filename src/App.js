3757a42a8338f455ed1c79be608e8be64948ebc3
gudtjr3437 <gudtjr3437@naver.com>
2021-11-16 21:38:49 +0900

    Feat : api test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import UserSetting from "./pages/UserSetting";
+import { useEffect } from "react";
+import api from "./apis/api";
+import { get } from "http";
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      const result = await api.getList();
+      console.log(result.data.RESULT);
+    };
+    getData();
+  }, []);
   return (
     <Routes>
       <Route exact path="/" element={<UserSetting />} />
