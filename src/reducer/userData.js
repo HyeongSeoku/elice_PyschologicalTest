0d52833a53a8b5cbb8da20aa935b1e153dc564ca
gudtjr3437 <gudtjr3437@naver.com>
2021-11-17 16:18:44 +0900

    Feat : action 수정

--- src/reducer/userData.js
+++ src/reducer/userData.js
@@ -1,19 +1,40 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
 
 //action
-export const setUser = createAction("SET_USER");
+export const setUserName = createAction("SET_USER_NAME");
+export const setUserGender = createAction("SET_USER_GENDER");
+export const setUserTestType = createAction("SET_USER_TEST_TYPE");
 export const resetUser = createAction("RESET_USER"); //유저 리셋
 
-const initState = { name: "", gender: "" };
+const initState = { name: "", gender: "", testType: "" };
 
 //sub reducer
 const user_reducer = createReducer(initState, {
-  [setUser]: (state, { payload }) => {
-    return { name: payload.name, gender: payload.gender };
+  [setUserName]: (state, { payload }) => {
+    return {
+      name: payload.name,
+      gender: state.gender,
+      testType: state.testType,
+    };
+  },
+  [setUserGender]: (state, { payload }) => {
+    return {
+      name: state.name,
+      gender: payload.gender,
+      testType: state.testType,
+    };
+  },
+  [setUserTestType]: (state, { payload }) => {
+    return {
+      name: state.name,
+      gender: state.gender,
+      testType: payload.testType,
+    };
   },
   [resetUser]: () => ({
     name: "",
     gender: "",
+    testType: "",
   }),
 });
 
--- src/store.js
+++ src/store.js
@@ -2,7 +2,11 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { createStore } from "redux";
 import { combineReducers } from "redux";
 import requestData_reducer, { dataInit } from "./reducer/requestData";
-import user_reducer, { setUser } from "./reducer/userData";
+import user_reducer, {
+  setUserName,
+  setUserGender,
+  setUserTestType,
+} from "./reducer/userData";
 
 const reducer = combineReducers({
   requestData: requestData_reducer,
@@ -13,7 +17,9 @@ const store = createStore(reducer, composeWithDevTools());
 
 //action 통합 관리
 export const actionCreators = {
-  setUser,
+  setUserName,
+  setUserGender,
+  setUserTestType,
 };
 
 store.subscribe(() => console.log("상태변경:", store.getState()));
