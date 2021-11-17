c6a8f388f6e4ef7c161d52eb76676f9acc06e254
gudtjr3437 <gudtjr3437@naver.com>
2021-11-17 22:09:05 +0900

    Fix : 페이지 랜더링 방식 변경

--- src/pages/UserSetting.jsx
+++ src/pages/UserSetting.jsx
@@ -1,25 +1,45 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-import UserForm from "../components/UserForm";
+import { connect } from "react-redux";
+import styled, { keyframes } from "styled-components";
+import UserGender from "../components/UserGender";
+import UserName from "../components/UserName";
+import UserTestType from "../components/UserTestType";
+
+//animation 부분
+const fadeIn = keyframes`
+  from{
+    opacity: 0
+  }
+  to{
+    opacity: 1
+  }
+`;
 
 const UserSettingContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 20px 20px;
+  animation-duration: 0.5s;
+  animation-timing-function: ease-out;
+  animation-name: ${fadeIn}; //애니메이션 연결
+  animation-fill-mode: forwards;
 `;
 
-const Title = styled.h2`
-  font-size: 35px;
+const Title = styled.h3`
+  font-size: 32px;
   margin-bottom: 40px;
 `;
 
 const UserSetting = () => {
   return (
     <UserSettingContainer>
-      <Title>직업가치관검사</Title>
-      <UserForm />
+      <UserName />
+      <UserGender />
+      <UserTestType />
     </UserSettingContainer>
   );
 };
